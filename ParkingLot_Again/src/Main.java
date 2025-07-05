aac939c27ce20cdabb1d7faaf3e41f932a648971
sitaramguduri <nagasita@buffalo.edu>
2025-07-04 21:22:54 -0400

    parking lot lld

--- ParkingLot_Again/src/Main.java
+++ ParkingLot_Again/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // here we initiallize the list of parking spots that both four wheeler and two wheeler has we also initialise the
+        // vehicle that comes at the gate here and send the manager factory for the class intialisation of entrance gate and then generate the ticket there.
+        //
     }
 }
